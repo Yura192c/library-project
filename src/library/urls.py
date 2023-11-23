c2e07fff29da4b8ead265c390fbd426f12b2b2fd
Yura192c <fpm.mironchiYU@bsu.by>
2023-11-23 18:18:36 +0300

    Add urls for Book views

---
+++ src/library/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import BookListCreateView, BookDetailView
+
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+]
