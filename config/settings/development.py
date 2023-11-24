f5a7d6cea74dd249ee9a5ce07d738716c67457ee
Yura192c <fpm.mironchiYU@bsu.by>
2023-11-25 00:55:08 +0300

    Add allowed hosts

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,4 @@
-from .base import *  
+from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--p02rl1njznq^!o3!3lxclao^w#7!rp3@m((b=7ew35j*7nm+w'
@@ -6,4 +6,4 @@ SECRET_KEY = 'django-insecure--p02rl1njznq^!o3!3lxclao^w#7!rp3@m((b=7ew35j*7nm+w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
