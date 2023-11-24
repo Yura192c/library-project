33d6df98af0517bb5ca7eeb911f8e2d237b2dde5
Yura192c <fpm.mironchiYU@bsu.by>
2023-11-25 01:51:24 +0300

    Add bug for part 3

--- src/user/models.py
+++ src/user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
